46acc6f33b5c379b3af7f208bd880230bf851b15
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-11-29 19:56:51 +0600

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+
+var num = prompt("Enter your number: ");
+
+if (num >= 80) {
+    console.log('You have got A+')
+}
+else if (num >= 70) {
+    console.log('You have got A')
+}
+else if (num >= 60) {
+    console.log('You have got A-')
+}
+else if (num >= 50) {
+    console.log('You have got B')
+}
+else if (num >= 40) {
+    console.log('You have got C')
+}
+else if (num >= 33) {
+    console.log('You have got D')
+}
+else{
+    console.log('Fail')
+}
+
+
+
+
---
+++ practice.js
@@ -0,0 +1,13 @@
+let a = {
+    b: 1,
+    c: '2',
+    d: false
+};
+
+let b = a;
+
+b.b = 2;
+b.c = '3';
+b.d = true;
+
+console.log(a.b, a.c, a.d);
