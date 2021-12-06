a088505956ec6eb861ba7425db18f95157d4f739
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-12-06 19:09:40 +0600

    if else

--- practice.js
+++ practice.js
@@ -12,3 +12,28 @@ b.d = true;
 
 // console.log(a.b, a.c, a.d);
 
+
+//if, else if, else -----------------------
+
+var num = 5;
+
+if (num % 2 == 0) {
+    // console.log('even');
+}
+
+else{
+    // console.log('odd')
+}
+
+var num = 0;
+
+if(num > 0){
+    console.log('positive')
+}
+else if(num < 0){
+    console.log('negative')
+}
+
+else {
+    console.log('Zero')
+}
